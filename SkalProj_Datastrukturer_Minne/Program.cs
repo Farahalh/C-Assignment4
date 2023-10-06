db5fb36b42563da494788ac5ad33def5771ff774
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-10-06 14:32:21 +0200

    Walk through of assignment, filling in the gaps and adding example solutions for further development.

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -22,7 +22,7 @@ namespace SkalProj_Datastrukturer_Minne
                     + "\n1. Examine a List"
                     + "\n2. Examine a Queue"
                     + "\n3. Examine a Stack"
-                    + "\n4. CheckParanthesis"
+                    + "\n4. CheckParantheses"
                     + "\n0. Exit the application");
                 char input = ' '; //Creates the character input to be used with the switch-case below.
                 try
@@ -46,7 +46,7 @@ namespace SkalProj_Datastrukturer_Minne
                         ExamineStack();
                         break;
                     case '4':
-                        CheckParanthesis();
+                        CheckParantheses();
                         break;
                     /*
                      * Extend the menu to include the recursive 
@@ -61,43 +61,43 @@ namespace SkalProj_Datastrukturer_Minne
                 }
             }
         }
-        /// <summary>
+        // <summary>
         /// Questions:
-        /// </summary>
-        /// <returns>
-        /// 1. How does the Stack and Heap Work?
-        /// - The stack is a pile where the item on top of the pile is executed first, 
-        /// for the second item on th epile to be executed the top item must be removed.
-        /// Basically you can not access everyting at the same time.
-        /// 
-        /// - The Heap is is a heap of items in a none specific order, where the executed item is picked from the heap.
-        /// There is no need for the first item to be removed in order for the next item to be executed. 
-        /// Here you can access everything at the same time.
-        /// 
-        /// 2. What are Value Types and Reference Types?
-        /// - Value Types include bool, byte, char, decimal, double, enum, float, int, long, sbyte, short, 
-        /// struct, uint, ulong, and ushort.
-        /// 
-        /// The Value Type directly contains the data.
-        /// 
-        /// - Reference Types are classes and onterfaces. Variables of reference types store references to their data 
-        /// (objects) in memory, so they do not directly contain the data. (An object of tyoe Object, string, or 
-        /// dynamic is also a reference type.
-        /// 
-        /// 3. The following methods generate different result. The first one returns 3, and the second one returns 4. Why?
-        /// (This questoins is referring to a specific code snippet)
-        ///- The first method is using Value Types that is why x = 3; and not y = x; y = 4;
-        ///As y is not overriding x, they do not affect each other because they are not pointing to the same object.
-        ///
-        /// - The second method is using Reference Types, and because both x and y are pointing to the same object,
-        /// y will override the value of x.
-        /// 
-        /// </returns>
+        // </summary>
+        // <returns>
+        // 1. How does the Stack and Heap Work?
+        // - The stack is a pile where the item on top of the pile is executed first,
+        // for the second item on th epile to be executed the top item must be removed.
+        // Basically you can not access everyting at the same time.
+        // 
+        // - The Heap is is a heap of items in a none specific order, where the executed item is picked from the heap.
+        // There is no need for the first item to be removed in order for the next item to be executed. 
+        // Here you can access everything at the same time.
+        // 
+        // 2. What are Value Types and Reference Types?
+        // - Value Types include bool, byte, char, decimal, double, enum, float, int, long, sbyte, short, 
+        // struct, uint, ulong, and ushort.
+        // 
+        // The Value Type directly contains the data.
+        // 
+        // - Reference Types are classes and onterfaces.Variables of reference types store references to their data 
+        // (objects) in memory, so they do not directly contain the data. (An object of tyoe Object, string, or
+        // dynamic is also a reference type.
+        // 
+        // 3. The following methods generate different result. The first one returns 3, and the second one returns 4. Why?
+        // (This questoins is referring to a specific code snippet)
+        //- The first method is using Value Types that is why x = 3; and not y = x; y = 4;
+        //As y is not overriding x, they do not affect each other because they are not pointing to the same object.
+        //
+        // - The second method is using Reference Types, and because both x and y are pointing to the same object,
+        // y will override the value of x.
+        // 
+        // </returns>
 
 
-        /// <summary>
-        /// Examines the datastructure List
-        /// </summary>
+        // <summary>
+        // Examines the datastructure List
+        // </summary>
 
         static void ExamineList()
         {
@@ -156,16 +156,21 @@ namespace SkalProj_Datastrukturer_Minne
             }
 
             //1.Complete the implementation of the ExamineList method so that the examination can be carried out.
+            //
             //2.When Does the List's Capacity Increase? (i.e., the size of the underlying array)
             //When inputs are added to the list.
+            //
             //3.By How Much Does the Capacity Increase?
-            //By the exact amount of input that is added to list.
+            //By double the exact amount of input that is added to list.
+            //
             //4.Why Doesn't the List's Capacity Increase at the Same Rate as Elements Are Added?
-            //-
+            //To save resources.
+            //
             //5.Does the Capacity Decrease When Elements Are Removed from the List?
-            //-
+            //No, not immediately.
+            //
             //6.When Is It Advantageous to Use a Custom Array Instead of a List?
-            //-
+            //When you know the exact amount of items in the array.
 
         }
 
@@ -205,6 +210,7 @@ namespace SkalProj_Datastrukturer_Minne
 
                 char operation = input[0]; //converting input into char literals
                 string item = input.Substring(1).Trim(); //inputing string into substring and trimming space
+
 
                 switch (operation)
                 {
@@ -273,7 +279,7 @@ namespace SkalProj_Datastrukturer_Minne
                         ExamineStack.Pop();//delete item
                         break;
                     case 'r':
-                        ReverseText();
+                        Console.WriteLine(ReverseString("Hello"));
                         break;
                     case '0':
                         return;
@@ -289,27 +295,48 @@ namespace SkalProj_Datastrukturer_Minne
             }
         }
 
-        static string ReverseText()
+        //Alternative solution:
+        static string ReverseString(string input)
         {
-            //Collect a string
-            //Turn string into an array
-            //Reverse order of array
-            //Create new string with array
-            //Return new string
+            Stack<char> charStack = new Stack<char>();
 
-            //To get string input back in reverse, press r + space + (insert string) + enter
-            string sentence = Console.ReadLine(); //Assignes string to user inpu,Ask for user input
-            char[] charArr = sentence.ToCharArray(); //converts string user input to char and then char to char array
-            Array.Reverse(charArr); // reverses char array
-
-            foreach (char c in charArr)
+            foreach (char c in input)
             {
-                Console.WriteLine(c); //displays user inout as a reversed char array
+                charStack.Push(c);
             }
 
-            return sentence;
+            char[] reversedChars = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                reversedChars[i] = charStack.Pop();
+            }
+
+            return new string(reversedChars);
         }
-        static void CheckParanthesis()
+
+        //static string ReverseText()
+        //{
+
+        //    //Collect a string
+        //    //Turn string into an array
+        //    //Reverse order of array
+        //    //Create new string with array
+        //    //Return new string
+
+        //    //To get string input back in reverse, press r + space + (insert string) + enter
+        //    string sentence = Console.ReadLine(); //Assignes string to user inpu,Ask for user input
+        //    char[] charArr = sentence.ToCharArray(); //converts string user input to char and then char to char array
+        //    Array.Reverse(charArr); // reverses char array
+
+        //    foreach (char c in charArr)
+        //    {
+        //        Console.WriteLine(c); //displays user inout as a reversed char array
+        //    }
+
+        //    return sentence;
+        //}
+        static void CheckParantheses()
         {
             /*
              * Use this method to check if the paranthesis in a string is Correct or incorrect.
@@ -320,7 +347,48 @@ namespace SkalProj_Datastrukturer_Minne
             //1. create a list or dictionary with correct and uncorrect input
             //2. convert user input to char array
             //3. loop through user input to find opening and matching closing parentheses 
-            //3. create a bool sating true or false based on user input
+            //4. create a bool sating true or false based on user input
+
+            //Example solution:
+            //static bool CheckParentheses(string input)
+            //{
+            //    Stack<char> parenthesesStack = new Stack<char>();
+
+            //    foreach (char character in input)
+            //    {
+            //        if (character == '(' || character == '{' || character == '[')
+            //        {
+            //            //If it´s an open parentheses, add it to the stack.
+            //            parenthesesStack.Push(character);
+            //        }
+            //        else if (character == ')' || character == '}' || character == ']')
+            //        {
+            //            //If it´s a closed parentheses, check if it matches with the last open parentheses.
+            //            if (parenthesesStack.Count == 0)
+            //            {
+            //                //If the stack is empty then there is no matching open parentheses.
+            //                return false;
+            //            }
+
+            //            char lastOpened = parenthesesStack.Pop();
+            //            if (!IsMatchingPair(lastOpened, character))
+            //            {
+            //                //If the closed parentheses does not match the latest open parentheses:
+            //                return false;
+            //            }
+            //        }
+            //    }
+
+            //    //The stack should be  empty if all open parentheses has a matching closed parentheses.
+            //    return parenthesesStack.Count == 0;
+            //}
+
+            //static bool IsMatchingPair(char opening, char closing)
+            //{
+            //    return (opening == '(' && closing == ')') ||
+            //           (opening == '{' && closing == '}') ||
+            //           (opening == '[' && closing == ']');
+            //}
         }
     }
 }
